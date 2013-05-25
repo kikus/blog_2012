b4a4ec3384226caff5e5849de4953306c46ef8eb
Sergio Ruiz <sejoruiz@gmail.com>
2013-05-25 11:47:57 +0200

    Version con environment para Postgress o SQLite

---
+++ config/mkconfig.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+cat > config.json <<EOF
+{
+"username": "$DATABASE_USER",
+"password": "$DATABASE_PASSWORD",
+"database": "$DATABASE_NAME",
+"host":
+ "$DATABASE_HOST",
+"dialect": "$DATABASE_DIALECT",
+"port":
+ "$DATABASE_PORT",
+"protocol": "$DATABASE_PROTOCOL",
+"storage": "$DATABASE_STORAGE",
+"omitNull": true
+}
+EOF
--- models/models.js
+++ models/models.js
@@ -1,11 +1,21 @@
 // Modelos ORM
 var path = require('path');
 var Sequelize = require('sequelize');
-
-// Para usar SQLite:
-var sequelize = new Sequelize(null,null,null,
-{dialect:"sqlite",
-storage:"blog.sqlite"});
+// Para usar SQLite o Postgress:
+var sequelize = new Sequelize(
+// variables de entorno para Postgress y SQLite
+process.env.DATABASE_NAME,
+process.env.DATABASE_USER,
+process.env.DATABASE_PASSWORD,
+{ dialect: process.env.DATABASE_DIALECT,
+	protocol: process.env.DATABASE_PROTOCOL,
+	port:
+	process.env.DATABASE_PORT,
+	host:
+	process.env.DATABASE_HOST,
+	storage: process.env.DATABASE_STORAGE,
+	omitNull: true
+});
 
 // Importar la definicion de la clase Post desde post.js.
 // Y que este modulo exporta la clase Post:
