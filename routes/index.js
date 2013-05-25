ec2f51f4b993d61109216aed9fc69b89d995ef31
Sergio Ruiz <sejoruiz@gmail.com>
2013-05-25 12:46:54 +0200

    Merged tema1 master: master now follows actual development

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
 /*
  * GET home page.
  */
-
+counter = require('./counter');
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blog SWCM', contador: counter.getCount()}); 	//renderiza index con la etiqueta title = 'Express', usa el motor EJS con la plantilla layout.ejs y index.ejs en el body
+  //res.redirect('/index.html');	// Las peticiones a / las redirigimos a index.ejs
+
 };
